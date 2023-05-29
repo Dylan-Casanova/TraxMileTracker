052bddd60cf32772258f3b204bf07b7781a29504
Dylan Casanova <dylan.casanova@amtil.co>
2023-05-29 15:52:21 -0500

    Cleaned out code, finished testing, finished routes. completed assessment

--- routes/api.php
+++ routes/api.php
@@ -17,41 +17,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+ return $request->user();
 })->middleware('auth:api');
 
 
-//////////////////////////////////////////////////////////////////////////
-/// Mock Endpoints To Be Replaced With RESTful API.
-/// - API implementation needs to return data in the format seen below.
-/// - Post data will be in the format seen below.
-/// - /resource/assets/traxAPI.js will have to be updated to align with
-///   the API implementation
-//////////////////////////////////////////////////////////////////////////
-
 // endpoint to get all cars for the logged in user
-
 Route::get('/get-cars', [CarsController::class, 'index'])->middleware('auth:api');
 
 // endpoint to add a new car.
-
 Route::post('/add-car', [CarsController::class, 'store'])->middleware('auth:api');
 
-Route::get('/get-car/{car}', [CarsController::class, 'show'])->middleware('auth:api');
-Route::delete('/delete-car/{car}', [CarsController::class, 'destroy'])->middleware('auth:api');
-
 // endpoint to get a car with the given id
-
-// Route::get('/get-car/{id}', [CarsController::class, 'show'])->middleware('auth:api');
+Route::get('/get-car/{car}', [CarsController::class, 'show'])->middleware('auth:api');
 
 // endpoint to delete a car with a given id
-
-// Route::delete('/delete-car/{id}', [CarsController::class, 'destroy'])->middleware('auth:api');
+Route::delete('/delete-car/{car}', [CarsController::class, 'destroy'])->middleware('auth:api');
 
 // endpoint to get the trips for the logged in user
-
 Route::get('/get-trips', [TripsController::class, 'index'])->middleware('auth:api');
 
 // endpoint to add a new trip.
-
 Route::post('/add-trip', [TripsController::class, 'store'])->middleware('auth:api');
